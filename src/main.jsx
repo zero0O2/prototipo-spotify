bc6c6c8ad3bee15db2789c19e092f5b5ec2dcc63
zero0O2 <148266981+zero0O2@users.noreply.github.com>
2026-01-09 13:21:08 -0300

    config deploy

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
-import { Routes , Route , BrowserRouter } from 'react-router-dom'
+import { Routes , Route , HashRouter} from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Home from './pages/Home.jsx'
 import ArtistsPage from './pages/ArtistsPage.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <HashRouter>
     <Routes>
       <Route path='/' element={<Home/>} />
       <Route path='/artista/:slug' element={<ArtistsPage/>} />
     </Routes>
-  </BrowserRouter>,
+  </HashRouter>,
 )
--- vite.config.js
+++ vite.config.js
@@ -8,5 +8,5 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
-  base:"/prototipo-spotify",
+  base:"/prototipo-spotify/",
 })
